685cadbed15076c6a6cf063ca17ac6144e0ebc21
Cody Shaner <cshaner04@gmail.com>
2018-04-26 22:35:11 -0700

    basic completion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,44 @@
-const modal = document.getElementById('myModal');
-const imgs = document.getElementsByClassName('image');
+const modal = document.getElementById("myModal");
+const imgs = document.getElementsByClassName("image");
 const modalImg = document.getElementById("modalImg");
 const captionText = document.getElementById("caption");
 const span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 for (var i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener('click', myFunction, false);
+  imgs[i].addEventListener("click", modalPop, false);
 }
 
-function myFunction() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+function modalPop() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
-console.log(window.location.href)
+
+/// Handle form submit
+/////////////////////////
+
+$("#contact-form").submit(function(e) {
+  let name = document.getElementById("name");
+  let email = document.getElementById("mail");
+  let msg = document.getElementById("message");
+
+  if (!name.value || !email.value || !msg.value) {
+    alert("Please complete all feilds");
+  } else {
+    $.ajax({
+      url: "https://formspree.io/cshaner04@gmail.com",
+      method: "POST",
+      data: $(this).serialize(),
+      dataType: "json"
+    });
+    e.preventDefault();
+    $(this)
+      .get(0)
+      .reset();
+    alert("Message sent");
+  }
+});
